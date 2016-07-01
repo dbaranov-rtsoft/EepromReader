f80780d53c9ea09daca2675e839854479d762368
dbaranov-rtsoft <dbaranov@dev.rtsoft.ru>
2016-07-01 08:41:10 +0300

    Code cleanup

---
+++ EepromReader/I2CEepromDevice.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <boost\noncopyable.hpp>
+
+class I2CEepromDevice : boost::noncopyable {
+	std::ifstream eepromInputfile;
+
+public:
+	explicit I2CEepromDevice(std::string eepromPath) : eepromInputfile(eepromPath, std::ios::binary) {};
+
+	template<typename T>
+	bool read(T& data);
+};
+
+template<typename T>
+bool I2CEepromDevice::read(T& data) {
+	static_assert(!std::is_pointer<T>::value, "data shouldn't be pointer");
+
+	if (eepromInputfile.bad()) {
+		return false;
+	}
+
+	eepromInputfile.read(reinterpret_cast<char*>(&data), sizeof(data));
+	return static_cast<bool>(eepromInputfile);
+}
--- EepromReader/main.cpp
+++ EepromReader/main.cpp
@@ -1,30 +1,16 @@
 #include <string>
 #include <iostream>
 #include <cstdint>
-#include "EepromDataReadingAdapter.h"
+#include "I2CEepromDevice.h"
 #include "EepromData.h"
 
 const std::string eepromFileName = "BinaryEeprom.txt";
 
-bool ReadEepromData(EepromDataReadingAdapter& eepromAdapter, EepromData& eepromData) noexcept [[suppress(bounds.2)]] {
-	for (size_t i = 0; i < sizeof(EepromData); i++) {
-		bool isOk = eepromAdapter.readByte(eepromData.bytes[i]);
-		if (!isOk) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
 int main() {
-	EepromDataReadingAdapter eepromAdapter(eepromFileName);
-	if (eepromAdapter.open() == false) {
-		std::cout << "Can`t open file " << eepromFileName << std::endl;
-	}
+	I2CEepromDevice eepromI2cDevice(eepromFileName);
 
 	EepromData eepromData;
-	bool isOk = ReadEepromData(eepromAdapter, eepromData);
+	bool isOk = eepromI2cDevice.read(eepromData);
 	if (!isOk) {
 		return -1;
 	}
